0c3ed6398eb7acab9e765a74615922be67f4954b
Seth Borne <sethborne@gmail.com>
2017-09-12 20:25:39 -0700

    Algorithms - Javascript - Chapter 02 - Page 30 - Progress II

--- Algorithm_Book_Coding_Dojo/01_JavaScript/Chapter_02/chapter_02_page_30.js
+++ Algorithm_Book_Coding_Dojo/01_JavaScript/Chapter_02/chapter_02_page_30.js
@@ -13,32 +13,56 @@ function twelveBarBlues(numValMin, numValMax){
 giveMeSpace();
 twelveBarBlues(1, 12);
 
-function fibonacci(numValFib){
-    var sum = 0;
+// function fibonacci(numValFib){
+//     var sum = 1;
     // var i = 1;
-    var prev = 1;
-    // while(numValFib > i){
-    //     console.log(`Iterator is: ${i}`);
+    // var prev = 0;
+    // var count = 0;
+    // while(numValFib-2 > count){
+    //     console.log(`Iterator is: ${count}`);
     //     sum += prev;
     //     console.log(`Sum is: ${sum}`);
-    //     i += 1;
+    //     count += 1;
     //     prev = sum;
     //     console.log(`Prev is: ${prev}`);
     // }
-    for(var i = 1; i < numValFib + 1; i += 1){
-        if(i == 1){
-            sum += prev;
-            console.log(`At ${i}, Sum: ${sum}`);
-            // prev = sum;
-            console.log(`At ${i}, Prev: ${prev}`);
+    // for(var i = 1; i < numValFib + 1; i += 1){
+    //     if(i == 1){
+    //         sum += prev;
+    //         console.log(`At ${i}, Sum: ${sum}`);
+    //         // prev = sum;
+    //         console.log(`At ${i}, Prev: ${prev}`);
+    //     }
+    //     else{
+    //         sum += prev
+    //         console.log(`At ${i}, Sum: ${sum}`);
+    //         prev = sum;
+    //         console.log(`At ${i}, Prev: ${prev}`);
+    //     }
+    // }
+// }
+
+// fibonacci(4)
+
+function fibonacci(numVal){
+    var newArr = [];
+    for(var idx = 0; idx < numVal + 1; idx++){
+        //populate each spot
+        if(idx >= 2){
+            //add
+            newArrVal = newArr[idx - 1] + newArr[idx - 2];
+            newArr.push(newArrVal)
         }
         else{
-            sum += prev
-            console.log(`At ${i}, Sum: ${sum}`);
-            prev = sum;
-            console.log(`At ${i}, Prev: ${prev}`);
+            newArr.push(idx);
         }
     }
+    console.log("The Index number " + numVal +  " of the Fib Sequence is " + newArr[numVal] );
 }
 
+fibonacci(9)
+// 8
+fibonacci(8)
+// 0
 fibonacci(5)
+// 5
