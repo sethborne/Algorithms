9328947e97543074cd091867c2b0e3c8a37cff8e
Seth Borne <sethborne@gmail.com>
2017-09-13 10:32:54 -0700

    Algorithms - Javascript - Chapter 02 - Page 30 - Progress III

--- Algorithm_Book_Coding_Dojo/01_JavaScript/Chapter_02/chapter_02_page_30.js
+++ Algorithm_Book_Coding_Dojo/01_JavaScript/Chapter_02/chapter_02_page_30.js
@@ -59,10 +59,66 @@ function fibonacci(numVal){
     }
     console.log("The Index number " + numVal +  " of the Fib Sequence is " + newArr[numVal] );
 }
-
+giveMeSpace();
 fibonacci(9)
 // 8
 fibonacci(8)
 // 0
 fibonacci(5)
 // 5
+
+function testModulo(numVal){
+    numTest = numVal % 10;
+    console.log(numTest);
+}
+giveMeSpace();
+testModulo(42)
+
+function sumToOneDigit(numVal){
+    var sum = 0;
+    var numToSum;
+    
+    while(numVal){
+        numToSum = numVal % 10;
+        sum += numToSum;
+        if(sum % 10 > 0){
+            numToSumEdge = sum % 10;
+            sum = Math.floor(sum / 10);
+            sum = sum + numToSumEdge
+        }
+        numVal = Math.floor(numVal / 10);
+    }
+    console.log(sum);
+}
+giveMeSpace();
+sumToOneDigit(100);
+sumToOneDigit(987);
+
+function isAPrime(numVal){
+    var isPrime = true;
+    for(var i = 2; i < numVal; i += 1){
+        console.log(`run loop ${i}`);
+        if(numVal % i == 0){
+            isPrime = false;
+            console.log("   break");
+        }
+    }
+    if(isPrime == true){
+        console.log(`${numVal} is a Prime Number`);
+    }
+    else{
+        console.log(`${numVal} is not a Prime Number`);
+    }
+}
+giveMeSpace();
+isAPrime(2);
+giveMeSpace();
+isAPrime(3);
+giveMeSpace();
+isAPrime(4);
+giveMeSpace();
+isAPrime(5);
+isAPrime(6);
+isAPrime(11);
+isAPrime(13);
+
