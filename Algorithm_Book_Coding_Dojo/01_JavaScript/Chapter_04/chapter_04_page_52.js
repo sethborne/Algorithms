40e054982e068714509a1f4878ebaaa8c0f044a5
Seth Borne <sethborne@gmail.com>
2017-09-26 11:48:38 -0700

    Algorithms - Javascript - Chapter 04 - Page 52 -  Second Commit

--- Algorithm_Book_Coding_Dojo/01_JavaScript/Chapter_04/chapter_04_page_52.js
+++ Algorithm_Book_Coding_Dojo/01_JavaScript/Chapter_04/chapter_04_page_52.js
@@ -46,14 +46,97 @@ parensValid(")()(")
 
 function bracesValid(inputString){
     var stack = [];
-    var size = 0;
-    var beginCap = ["(", "[", "{"];
-    var endCap = [")", "]", "}"];
+    var open = {
+        "{": "}",
+        "[": "]",
+        "(": ")"
+    };
+    var closed = {
+        "}": true,
+        "]": true,
+        ")": true
+    };
+    var isValid = true;
+    
     for(var i = 0; i < inputString.length; i += 1){
-        
+        var char = inputString[i];
+        if(open[char]){
+            stack.push(char);
+            console.log(`At Loop: ${i} | closed array object key: ${char} | stack: ${stack}`);
+        }
+        // when you hit a closing char, this will always run.
+        else if(closed[char]){
+            // key of open array object
+            var keyOpen = stack[stack.length - 1];
+            var valueOpen = open[stack[stack.length - 1]];
+            console.log(`At Loop: ${i} | closed array object key: ${char} | open array object value: ${valueOpen} | open array object key: ${keyOpen} | stack: ${stack}`);
+            if(valueOpen === char){
+                stack.pop();
+                console.log(`At Loop: ${i} | post pop | stack value: ${valueOpen} popped | stack: ${stack}`);
+            }
+            else{
+                isValid = false;
+                // break;
+            }
+        }
     }
+    console.log(stack.length);
+    if(stack.length > 0){
+        isValid = false;
+    }
+    console.log(`The String: ${inputString} | isValid: ${isValid} | stack: ${stack} | stackLength: ${stack.length}`);
+    return isValid;
+}
+
+giveMeSpace();
+// true
+bracesValid("W(a{t}s[o(n{ c}o)m]e )h[e{r}e]");
+giveMeSpace();
+// false
+bracesValid("D(i{a}l[ t]o)n{e");
+giveMeSpace();
+// false
+bracesValid("A(1)s[O(n]0{t 0}k")
+giveMeSpace();
+// false
+bracesValid("{[}]")
+
+function bracesValid2(inputString){
+    console.log(inputString);
+    var pairs={
+        "(" : ")",
+        "{" : "}",
+        "[" : "]"
+    }
+    var stack=[]
+    for(var i = 0; i < inputString.length; i += 1){
+        if(inputString[i] in pairs){
+            stack.push(inputString[i]);
+            console.log(stack);
+        }
+        else {
+            var current = stack.pop();
+            if(pairs[current] !== inputString[i]){
+                console.log("FALSE");
+                return false
+            }
+        }
+    }
+    console.log(stack.length);
+    return stack.length === 0
 }
 giveMeSpace();
+// true
+bracesValid2("W(a{t}s[o(n{ c}o)m]e )h[e{r}e]");
+giveMeSpace();
+// false
+bracesValid2("D(i{a}l[ t]o)n{e");
+giveMeSpace();
+// false
+bracesValid2("A(1)s[O(n]0{t 0}k")
+giveMeSpace();
+// false
+bracesValid2("{[}]")
 
 
 function isPalindrome(inputString){
@@ -64,7 +147,7 @@ function isPalindrome(inputString){
     
     var isPalindrome = true;
     
-    console.log(half);
+    // console.log(half);
     for(var end = splitInputString.length - 1; end > start; end -= 1){
         // console.log(`Loop: ${end}: End: ${half}: Start: ${start}`);
         // console.log(splitInputString[end]);
@@ -96,3 +179,28 @@ isPalindrome("Racecar")
 function longestPalindrome(inputString){
     
 }
+
+longestPalindrome("what up, daddy-o");
+longestPalindrome("uh... not much");
+longestPalindrome("Yikes! my favorite racecar erupted")
+
+// function test(){
+//     array = { 
+//                 key1: "value1",
+//                 key2: "value2"
+//             }
+//     array['key2'] = "simon"
+//     array['key3'] = "hello"
+    
+//     array.key2 = "simon 3"
+//     array.key4 = "worst"
+    
+//     console.log(array['key'+2]);
+//     console.log(array);
+    
+//     var key2 = array.key2;
+//     console.log(`Second: ${key2}`);
+    
+// }
+
+// test()
