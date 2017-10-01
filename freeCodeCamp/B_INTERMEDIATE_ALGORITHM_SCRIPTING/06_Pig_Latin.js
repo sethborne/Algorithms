5ef8bc1ea0cc1b75d400d742563eb7319b96de95
Seth Borne <sethborne@gmail.com>
2017-10-01 08:30:25 -0700

    Algorithms - freeCodeCamp - Intermediate Algorithms - 06 - Pig Latin - Final Commit2

--- freeCodeCamp/B_INTERMEDIATE_ALGORITHM_SCRIPTING/06_Pig_Latin.js
+++ freeCodeCamp/B_INTERMEDIATE_ALGORITHM_SCRIPTING/06_Pig_Latin.js
@@ -11,14 +11,12 @@ function translatePigLatin(inputString) {
         splitInputString.push("w");
         splitInputString.push("a");
         splitInputString.push("y");
-        // console.log(splitInputString);
     }
     else{
         var foundFirstVowel = false;
         var vowelIndex;
         for(var v = 0; v < splitInputString.length - 1; v += 1){
             if(foundFirstVowel == false){
-                // console.log(v);
                 if(splitInputString[v] === "a" || splitInputString[v] === "e" || splitInputString[v] === "i" || splitInputString[v] === "o" || splitInputString[v] === "u"){
                     vowelIndex = v;
                     console.log(vowelIndex);
@@ -36,14 +34,14 @@ function translatePigLatin(inputString) {
             for(var j = 0; j < splitInputString.length; j += 1){
                 splitInputString[j] = splitInputString[j + 1];
             }
-            splitInputString.pop(splitInputString[splitInputString.length - 1])
+            splitInputString.pop(splitInputString[splitInputString.length - 1]);
         }
         console.log(tempCharArray);
         for(var n = 0; n < tempCharArray.length; n += 1){
             splitInputString.push(tempCharArray[n]);
         }
-        splitInputString.push("y");
         splitInputString.push("a");
+        splitInputString.push("y");
         console.log(splitInputString);
     }
     inputString = splitInputString.join("");
