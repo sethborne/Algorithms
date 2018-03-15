e4062ba98daaedc4a06ad68fd1d3b025577485aa
Seth Borne <sethborne@gmail.com>
2018-03-14 21:23:58 -0700

    Algorithms - Javascript - Chapter 06 - Page 74 - Update Commit

--- Algorithm_Book_Coding_Dojo/01_JavaScript/Chapter_06/chapter_06_page_74.js
+++ Algorithm_Book_Coding_Dojo/01_JavaScript/Chapter_06/chapter_06_page_74.js
@@ -38,3 +38,85 @@ console.log(stack.pop());
 console.log(stack.contains(30));
 console.log(stack.size());
 console.log(stack);
+
+function SinglyLinkedList(){
+    this.head = null;
+}
+
+function Node(value){
+    this.value = value;
+    this.next = null;
+}
+
+SinglyLinkedList.prototype = {
+    push: function(value){
+        // push should add to the front
+        let addNode = new Node(value);
+        if(!this.head){
+            this.head = addNode;
+        }
+        else{
+            addNode.next = this.head;
+            this.head = addNode;
+        }
+    },
+    pop: function(){
+        // need to remove the head and return it
+        let removeNode = this.head;
+        this.head = removeNode.next;
+        removeNode.next = null;
+        return removeNode;
+    },
+    top: function(){
+        // return the head
+        return this.head;
+    },
+    contains: function(value){
+        let doesContain = false;
+        let currentNode = this.head;
+        while(currentNode){
+            if(currentNode.value === value){
+                doesContain = true;
+                return doesContain;
+            }
+            else {
+                currentNode = currentNode.next;
+            }
+        }
+        doesContain = false;
+        return doesContain
+    },
+    isEmpty: function(){
+        // if there is no head
+        return !this.head ? true : false;
+    },
+    size: function(){
+        // set a count and loop through
+        let size = 0;
+        let currentNode = this.head;
+        while(currentNode){
+            size += 1;
+            currentNode = currentNode.next;
+        }
+        return size;
+    }
+    
+}
+
+let SLL = new SinglyLinkedList();
+SLL.push(10);
+SLL.push(15);
+SLL.push(20);
+SLL.push(25);
+SLL.push(30);
+SLL.push(35);
+SLL.push(40);
+console.log(SLL);
+console.log(SLL.pop());
+console.log(SLL.top());
+console.log(SLL.contains(10));
+console.log(SLL.contains(15));
+console.log(SLL.contains(25));
+console.log(SLL.isEmpty());
+console.log(SLL.size());
+console.log(SLL);
